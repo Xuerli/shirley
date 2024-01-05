7ffd1e82f3ac4424c3aa69a47f7fce4e107873b5
Xue Li <xli3310@ed.ac.uk>
2024-01-05 16:24:06 +0000

    Update index.tsx
    Signed-off-by: Xue Li <xli3310@ed.ac.uk>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,13 @@
-import {FC, memo} from 'react';
 import dynamic from 'next/dynamic';
+import {FC, memo} from 'react';
 
 import Page from '../components/Layout/Page';
 import About from '../components/Sections/About';
 import Contact from '../components/Sections/Contact';
 import Footer from '../components/Sections/Footer';
 import Hero from '../components/Sections/Hero';
-import Resume from '../components/Sections/Resume';
 import Research from '../components/Sections/Research';
+import Resume from '../components/Sections/Resume';
 import Testimonials from '../components/Sections/Testimonials';
 import {homePageMeta} from '../data/data';
 
