fda1f6a0c7540db70a0da0606523c6015c88059e
Xue Li <xli3310@ed.ac.uk>
2024-01-05 18:19:24 +0000

    Add files via upload
    Signed-off-by: Xue Li <xli3310@ed.ac.uk>

---
+++ src/components/Sections/Research.tsx
@@ -0,0 +1,40 @@
+import {FC, memo} from 'react';
+
+import {otherActivities, projects, publications, SectionId, supervision,} from '../../data/data';
+import PublicationItems from '../Layout/PublicationItems';
+import Section from '../Layout/Section';
+import SSSection from '../Layout/SSSection';
+import TimelineItem from '../Layout/TimelineItem';
+
+
+const Research: FC = memo(() => {
+  return (
+    <Section className="bg-neutral-100" sectionId={SectionId.Research}>
+      <div className="flex flex-col divide-y-2 divide-neutral-300">
+        <SSSection title="Projects">
+          {projects.map((item, index) => (
+            <TimelineItem item={item} key={`${item.title}-${index}`} />
+          ))}
+        </SSSection>
+        <SSSection title="Supervision">
+          {supervision.map((item, index) => (
+            <TimelineItem item={item} key={`${item.title}-${index}`} />
+          ))}
+        </SSSection>
+        <SSSection title="Other Activities">
+          {otherActivities.map((item, index) => (
+            <TimelineItem item={item} key={`${item.title}-${index}`} />
+          ))}
+        </SSSection>
+        <SSSection title="Publications">
+          {publications.map((item, index) => (
+            <PublicationItems item={item} key={`${item.title}-${index}`} />
+          ))}
+        </SSSection>
+      </div>
+    </Section>
+  );
+});
+
+Research.displayName = 'Research';
+export default Research;
---
+++ src/components/Sections/Resume/index.tsx
@@ -0,0 +1,36 @@
+import {FC, memo} from 'react';
+
+import {education, experience, SectionId, skills} from '../../../data/data';
+import Section from '../../Layout/Section';
+import {SkillGroup} from './Skills';
+import SSSection from '../../Layout/SSSection';
+import TimelineItem from '../../Layout/TimelineItem';
+
+const Resume: FC = memo(() => {
+  return (
+    <Section className="bg-neutral-100" sectionId={SectionId.Resume}>
+      <div className="flex flex-col divide-y-2 divide-neutral-300">
+        <SSSection title="Education">
+          {education.map((item, index) => (
+            <TimelineItem item={item} key={`${item.title}-${index}`} />
+          ))}
+        </SSSection>
+        <SSSection title="Experience">
+          {experience.map((item, index) => (
+            <TimelineItem item={item} key={`${item.title}-${index}`} />
+          ))}
+        </SSSection>
+        <SSSection title="Skills">
+          <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
+            {skills.map((skillgroup, index) => (
+              <SkillGroup key={`${skillgroup.name}-${index}`} skillGroup={skillgroup} />
+            ))}
+          </div>
+        </SSSection>
+      </div>
+    </Section>
+  );
+});
+
+Resume.displayName = 'Resume';
+export default Resume;
